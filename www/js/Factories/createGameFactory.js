0196fba55df11b862e9dbf71d6fa0354bf8e75d5
Wisbell <westleyisbell@gmail.com>
2017-05-18 15:15:47 -0500

    Modularized the factories

--- www/js/Factories/createGameFactory.js
+++ www/js/Factories/createGameFactory.js
@@ -1,32 +1,35 @@
-angular.module('starter.factories', [])
+scoreKeeper
 
+// Create Game Factory
 .factory('CreateGameFactory', function($http){
 
   return {
             getGameSettingsList: function(){
                       return gameSettingsRef.once('value')
-                        .then((snap) => snap.val())
-                        .then((gameSettingsObject) => {
-                          console.log(gameSettingsObject)
+                        .then(function(snap) { return snap.val() })
+                        .then(function(gameSettingsObject) {
+                          return gameSettingsObject
                         })
-                      })
+                    },
+
+            getGameNameList: function(){
+                      return gameSettingsRef.once('value')
+                        .then(function(snap) { return snap.val() })
+                        .then(function(gameSettingsObject) {
+
+                          var nameList = []
+
+                          for(var obj in gameSettingsObject) {
+                            nameList.push(gameSettingsObject[obj].gameTypeName)
+                          }
+
+                          return nameList
+                        })
+                    },
+            createNewGame: function(createGame){
+                      console.log("createNewGame function called from factory")
+                      console.log("create game object", createGame)
+                      return currentGamesRef.push(createGame)
                     }
-          }
+          } // Close factory return statement
 })
-
-
-
-
-
-// app.factory('DoctorFactory', function($http){
-
-//   return {
-//             getDoctorList: function(){
-//               return $http.get('https://west-doctors-patients.firebaseio.com/doctors.json')
-//                 .then(function(val){
-//                   console.log("val", val)
-//                   return val.data
-//               })
-//             }
-//           }
-// })
